b08d08bef929642f0371b56fe817797bac80b52e
occiandiaali <ocean.diaali@gmail.com>
2024-02-26 10:47:33 +0100

    add temp sign in check and replace onboard text

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -15,20 +15,20 @@
       style="height: 64px;max-width: 150px;border-radius: 100%;margin: 16px;"
       alt="easy-shop"
       />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
+          <div class="q-mt-md text-center text-body1">
+            {{ slide1Text }}
           </div>
         </q-carousel-slide>
         <q-carousel-slide name="tv" class="column no-wrap flex-center">
-          <q-icon name="live_tv" color="primary" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
+          <q-icon name="qr_code" color="accent" size="56px" />
+          <div class="q-mt-md text-center text-body1">
+            {{ slide2Text }}
           </div>
         </q-carousel-slide>
         <q-carousel-slide name="layers" class="column no-wrap flex-center">
-          <q-icon name="layers" color="primary" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
+          <q-icon name="layers" color="accent" size="56px" />
+          <div class="q-mt-md text-center text-body1">
+            {{ slide3Text }}
           </div>
         </q-carousel-slide>
         <q-carousel-slide name="map" class="column no-wrap flex-center">
@@ -41,7 +41,7 @@
             
             <q-input outlined v-model="username" label="Username" /><br/>
             <q-input outlined v-model="email" label="Email" /><br/>
-            <q-btn :disable="!username && !email" color="purple" label="Continue" @click="onContinue"/>
+            <q-btn :disable="(!email || !username)" color="purple" label="Continue" @click="onContinue"/>
           </div>
         </q-carousel-slide>
       </q-carousel>
@@ -71,9 +71,11 @@
   const slide = ref('style');
   const username = ref('')
   const email = ref('')
-  const lorem = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo provident incidunt ducimus iusto perferendis porro earum. Totam, numquam?';
-  //let btnDisabled = ref(username.value === '' && email.value === '')
+ // const lorem = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo provident incidunt ducimus iusto perferendis porro earum. Totam, numquam?';
 
+  const slide1Text = 'EasyShop makes your shopping experience..easier. Enjoy in-store shopping without the stress of standing on a queue when it is time to checkout.';
+  const slide2Text = 'Scan the QR Code for the item you want to buy to add it to your cart automatically. Scan each item just once, then increment the quantity you want on your cart page.';
+  const slide3Text = 'No need to stand in line for even one second to pay for your shopping. Use the in-app Paystack feature to make payment by card or transfer. Checkout your shopping in less than 15 seconds, with your receipt sent to the email you provide. Fast!'
   function onContinue() {
     localStorage.setItem('username', username.value);
     localStorage.setItem('useremail', email.value);
